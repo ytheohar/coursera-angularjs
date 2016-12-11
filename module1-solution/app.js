8db2a281ffd25c5109c12478ef9dd738a003b864
Yannis Theocharis <ytheohar@gmail.com>
2016-12-11 19:28:26 +0000

    1st Assignment

---
+++ module1-solution/app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.dishes;
+  $scope.msg;
+
+  $scope.checkIfTooMuch = function() {
+    var length = $scope.dishes.length;
+    if (length === 0) {
+      $scope.msg = 'Please enter data first';
+    } else if (length <= 3) {
+      $scope.msg = 'Enjoy!';
+    } else {
+      $scope.msg = 'Too much!';
+    }
+  };
+
+}
+
+
+})();
